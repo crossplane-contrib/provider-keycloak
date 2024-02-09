341521a4eb154317faf5fe6a81f1d729844af3e7
Breee <julian@corewire.de>
2024-02-09 13:01:24 +0100

    cc: fix linter issues

--- config/openidclient/config.go
+++ config/openidclient/config.go
@@ -2,28 +2,33 @@ package openidclient
 
 import "github.com/crossplane/upjet/pkg/config"
 
+const (
+	// Group is the short group for this provider.
+	Group = "openidclient"
+)
+
 // Configure configures individual resources by adding custom ResourceConfigurators.
 func Configure(p *config.Provider) {
 	p.AddResourceConfigurator("keycloak_openid_client", func(r *config.Resource) {
 		// We need to override the default group that upjet generated for
-		r.ShortGroup = "openidclient"
+		r.ShortGroup = Group
 	})
 
 	p.AddResourceConfigurator("keycloak_openid_client_default_scopes", func(r *config.Resource) {
 		// We need to override the default group that upjet generated for
-		r.ShortGroup = "openidclient"
+		r.ShortGroup = Group
 	})
 
 	p.AddResourceConfigurator("keycloak_openid_client_scope", func(r *config.Resource) {
 		// We need to override the default group that upjet generated for
-		r.ShortGroup = "openidclient"
+		r.ShortGroup = Group
 	})
 
 	p.AddResourceConfigurator("keycloak_openid_client_service_account_role", func(r *config.Resource) {
-		r.ShortGroup = "openidclient"
+		r.ShortGroup = Group
 	})
 
 	p.AddResourceConfigurator("keycloak_openid_client_service_account_realm_role", func(r *config.Resource) {
-		r.ShortGroup = "openidclient"
+		r.ShortGroup = Group
 	})
 }
