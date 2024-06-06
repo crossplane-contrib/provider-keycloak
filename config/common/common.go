6b01f9d386a8fbc1ec210836c901a0b989828045
b509 <b509@users.noreply.github.com>
2024-06-06 16:16:35 +0200

    Feature/add flow reference to OIDC identity provider (#2)
    * Extract right field from authentication flow for binding referencing flow
    
    * Be able to reference execution from execution-config
    
    ---------
    
    Co-authored-by: Isabella Dix <isabella.dix@merckgroup.com>
    Co-authored-by: Dennis Kniep <kniepdennis@gmail.com>

--- config/common/common.go
+++ config/common/common.go
@@ -13,6 +13,9 @@ const (
 	// PathServiceAccountRoleIDExtractor is the golang path to ARNExtractor function
 	// in this package.
 	PathServiceAccountRoleIDExtractor = SelfPackagePath + ".ServiceAccountRoleIDExtractor()"
+	// PathAuthenticationFlowAliasExtractor is the golang path to ARNExtractor function
+	// in this package.
+	PathAuthenticationFlowAliasExtractor = SelfPackagePath + ".AuthenticationFlowAliasExtractor()"
 )
 
 // ServiceAccountRoleIDExtractor returns a reference.ExtractValueFn that can be used to extract the ServiceAccountRoleID from a managed resource.
@@ -24,6 +27,24 @@ func ServiceAccountRoleIDExtractor() reference.ExtractValueFn {
 			return ""
 		}
 		r, err := paved.GetString("status.atProvider.serviceAccountUserId")
+		if err != nil {
+			// todo(hasan): should we log this error?
+			return ""
+		}
+		return r
+	}
+}
+
+// AuthenticationFlowAliasExtractor extract Alias from AuthenticationFlow Ref
+func AuthenticationFlowAliasExtractor() reference.ExtractValueFn {
+	return func(mg xpresource.Managed) string {
+		paved, err := fieldpath.PaveObject(mg)
+		if err != nil {
+			// todo(hasan): should we log this error?
+			return ""
+		}
+		// Caution, this is case-sensitive
+		r, err := paved.GetString("status.atProvider.alias")
 		if err != nil {
 			// todo(hasan): should we log this error?
 			return ""
