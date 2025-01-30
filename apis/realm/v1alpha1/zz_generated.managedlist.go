cc148c0fde165d98c199d236edbc248fe34cb322
Dennis Kniep <kniepdennis@gmail.com>
2025-01-30 09:16:27 +0100

    feat: Added default & optional realm-client-scopes
    New realm related resources:
    * DefaultClientScopes
    * OptionalClientScopes
    
    Signed-off-by: Dennis Kniep <kniepdennis@gmail.com>

--- apis/realm/v1alpha1/zz_generated.managedlist.go
+++ apis/realm/v1alpha1/zz_generated.managedlist.go
@@ -7,8 +7,26 @@ package v1alpha1
 
 import resource "github.com/crossplane/crossplane-runtime/pkg/resource"
 
+// GetItems of this DefaultClientScopesList.
+func (l *DefaultClientScopesList) GetItems() []resource.Managed {
+	items := make([]resource.Managed, len(l.Items))
+	for i := range l.Items {
+		items[i] = &l.Items[i]
+	}
+	return items
+}
+
 // GetItems of this KeystoreRsaList.
 func (l *KeystoreRsaList) GetItems() []resource.Managed {
+	items := make([]resource.Managed, len(l.Items))
+	for i := range l.Items {
+		items[i] = &l.Items[i]
+	}
+	return items
+}
+
+// GetItems of this OptionalClientScopesList.
+func (l *OptionalClientScopesList) GetItems() []resource.Managed {
 	items := make([]resource.Managed, len(l.Items))
 	for i := range l.Items {
 		items[i] = &l.Items[i]
