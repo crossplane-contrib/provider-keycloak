c7f160340d8421f6a08c5b9c3a1e36b8ea6dc3d5
Dennis Kniep <kniepdennis@gmail.com>
2025-02-27 22:25:22 +0100

    test: e2e test cases
    import test cases:
    * state and status.id is removed, external-name is left (already
      existed as upbound test case)
    * state and status.id is removed, external-name is removed
    * state and status.id is removed, external-name is set to "Incorrect"
    
    change test cases order to delete realm at the end
    
    Signed-off-by: Dennis Kniep <kniepdennis@gmail.com>

---
+++ cluster/test/hack/patchIncorrectExternalName.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Original Script: https://raw.githubusercontent.com/crossplane/uptest/refs/heads/main/hack/patch.sh
+# Added: set annotation crossplane.io/external-name to Incorrect
+function patch {
+    kindgroup=$1;
+    name=$2;
+    if ${KUBECTL} --subresource=status patch "$kindgroup/$name" --type=merge -p '{"status":{"conditions":[]}}' ; then
+        return 0;
+    else
+        return 1;
+    fi;
+};
+
+
+kindgroup=$1;
+name=$2;
+attempt=1;
+max_attempts=10;
+while [[ $attempt -le $max_attempts ]]; do
+    if patch "$kindgroup" "$name"; then
+        echo "Successfully patched $kindgroup/$name";
+        ${KUBECTL} annotate "$kindgroup/$name" uptest-old-id=$(${KUBECTL} get "$kindgroup/$name" -o=jsonpath='{.status.atProvider.id}') --overwrite;
+        ${KUBECTL} annotate "$kindgroup/$name" crossplane.io/external-name=Incorrect --overwrite;
+        break;
+    else
+        printf "Retrying... (%d/%d) for %s/%s\n" "$attempt" "$max_attempts" "$kindgroup" "$name" >&2;
+    fi;
+    ((attempt++));
+    sleep 5;
+done;
+if [[ $attempt -gt $max_attempts ]]; then
+    echo "Failed to patch $kindgroup/$name after $max_attempts attempts";
+    exit 1;
+fi;
+exit 0;
--- cluster/test/setup.sh
+++ cluster/test/setup.sh
@@ -5,3 +5,26 @@ SCRIPT_DIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
 echo "Run Setup..."
 
 ${KUBECTL} apply -f ${SCRIPT_DIR}/../../dev/demos/basic/000-init.yaml
+
+# Uptest creates by default these chainsaw test files in following folder /tmp/uptest-e2e/case/
+# * 00-apply.yaml
+# * 02-import.yaml
+# * 03-delete.yaml
+
+# We want to add more import tests that:
+# 1. test if it finds the resource if the external name is set to a different value
+# 2. test if it finds the resource if the external name is not set
+
+cp ${SCRIPT_DIR}/hack/patchIncorrectExternalName.sh /tmp/patchIncorrectExternalName.sh
+sed "s/patch.sh/patchIncorrectExternalName.sh/g" 02-import.yaml | sed "s/curl/#curl/g" > 02-import-IncorrectExtName.yaml
+
+cp ${SCRIPT_DIR}/hack/patchRemoveExternalName.sh /tmp/patchRemoveExternalName.sh
+sed "s/patch.sh/patchRemoveExternalName.sh/g" 02-import.yaml | sed "s/curl/#curl/g" > 02-import-NoExtName.yaml
+
+echo "---" >> 02-import.yaml
+cat 02-import-IncorrectExtName.yaml >> 02-import.yaml
+echo "---" >> 02-import.yaml
+cat 02-import-NoExtName.yaml >> 02-import.yaml
+
+rm 02-import-IncorrectExtName.yaml
+rm 02-import-NoExtName.yaml
