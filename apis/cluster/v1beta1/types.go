66a4c58d0019aa018e87115e1f4629f8558c67d8
Vladimír Dudr <dudr@metrans.cz>
2025-11-07 21:30:05 +0100

    fix: remove unusable enum elements
    Signed-off-by: Vladimír Dudr <dudr@metrans.cz>

--- apis/cluster/v1beta1/types.go
+++ apis/cluster/v1beta1/types.go
@@ -20,7 +20,7 @@ type ProviderConfigSpec struct {
 // ProviderCredentials required to authenticate.
 type ProviderCredentials struct {
 	// Source of the provider credentials.
-	// +kubebuilder:validation:Enum=None;Secret;InjectedIdentity;Environment;Filesystem;Kubernetes
+	// +kubebuilder:validation:Enum=None;Secret;Environment;Filesystem
 	Source xpv1.CredentialsSource `json:"source"`
 
 	xpv1.CommonCredentialSelectors `json:",inline"`
